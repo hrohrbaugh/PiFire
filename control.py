66d8e2aa4540376477e16f69aa6f3f55c7a9a76a
Heath Rohrbaugh <heathrohrbaugh@gmail.com>
2025-12-26 22:58:29 -0500

    Added velocity pid option to controllers

--- control.py
+++ control.py
@@ -426,11 +426,22 @@ def _work_cycle(mode, grill_platform, probe_complex, display_device, dist_device
 		write_metrics(metrics)
 
 	if mode == 'Hold':
-		# Initialize cycle to minimum ratio.
-		OnTime = settings['cycle_data']['HoldCycleTime'] * settings['cycle_data']['u_min']  # Auger On Time
-		OffTime = settings['cycle_data']['HoldCycleTime'] * (1 - settings['cycle_data']['u_min'])  # Auger Off Time
+		# Load total cycle time from settings
 		CycleTime = settings['cycle_data']['HoldCycleTime']  # Total Cycle Time
-		CycleRatio = RawCycleRatio = settings['cycle_data']['u_min']  # Ratio of OnTime to CycleTime
+
+		# NOTE: We are intentionally NOT forcing CycleRatio to u_min here anymore if the vel_pid is selected
+		if settings['controller']['selected'] == 'vel_pid':
+			if 'CycleRatio' not in locals():
+				CycleRatio = settings['cycle_data']['u_min']
+			if 'RawCycleRatio' not in locals():
+				RawCycleRatio = CycleRatio
+		else:
+			CycleRatio = RawCycleRatio = settings['cycle_data']['u_min']
+
+		# Derive OnTime/OffTime from the preserved ratio
+		OnTime = CycleTime * CycleRatio
+		OffTime = CycleTime * (1 - CycleRatio)
+
 		LidOpenDetect = False
 		LidOpenEventExpires = 0
 		'''
@@ -439,6 +450,11 @@ def _work_cycle(mode, grill_platform, probe_complex, display_device, dist_device
 		controllerCore, controller_status = _init_controller(settings, control)
 		if controller_status == 'Inactive':
 			status = 'Inactive'
+		
+		# --- Velocity-PID bumpless init helpers ---
+		# Seed from the currently applied output
+		controller_seed_u = CycleRatio
+		controller_needs_init = True
 		eventLogger.debug('On Time = ' + str(OnTime) + ', OffTime = ' + str(OffTime) + ', CycleTime = ' + str(
 			CycleTime) + ', CycleRatio = ' + str(CycleRatio))
 
@@ -606,11 +622,19 @@ def _work_cycle(mode, grill_platform, probe_complex, display_device, dist_device
 		if control['controller_update'] and mode == 'Hold':
 			control['controller_update'] = False
 			write_control(control, direct_write=True, origin='control')
+
+			# --- Velocity-PID: Capture current applied output BEFORE swapping controllers---
+			prev_cycle_ratio = CycleRatio
+
 			# Reinitialize the controller with the updated settings
 			settings = read_settings()
 			controllerCore, controller_status = _init_controller(settings, control)
 			if controller_status == 'Active':
 				eventLogger.info('Controller reinitialized with updated settings')
+
+				# ---Velocity-PID: seed the new controller with previous output
+				controller_seed_u = prev_cycle_ratio
+				controller_needs_init = True
 
 		# Check if user changed hopper levels and update if required
 		if control['distance_update']:
@@ -707,6 +731,16 @@ def _work_cycle(mode, grill_platform, probe_complex, display_device, dist_device
 			if mode == 'Hold':
 				# Check to see if it's time to update pid and update if needed.
 				if (now - controllerCycleStart) > CycleTime:
+
+					# --- Velocity-pid: One-time initialization for vel-pid style controller ---
+					if controller_needs_init and hasattr(controllerCore, 'initialize'):
+						try:
+							controllerCore.initialize(u_init=controller_seed_u, pv_init=ptemp)
+							eventLogger.debug(f'Controller initialized (bumpless): u_init={controller_seed_u}, pv_init={ptemp}')
+						except Exception:
+							controlLogger.exception('Controller initialize() failed; continuing without bumpless init.')
+						controller_needs_init = False
+						
 					pid_output = controllerCore.update(ptemp)
 					controllerCycleStart = now
 					CycleRatio = RawCycleRatio = settings['cycle_data']['u_min'] if LidOpenDetect else pid_output
---
+++ controller/vel_pid.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python3
+
+'''
+*****************************************
+ PiFire PID Controller
+*****************************************
+
+ Description: This object will be used to calculate PID for maintaining
+ temperature in the grill.  This controller is based on the velocity form of the PID equation instead of the positional form
+ When the output is saturated (either below 0 or above 1) then we do not increase the integral output.
+ https://info.erdosmiller.com/blog/pid-anti-windup-techniques
+
+ This controller was originally developed by GitHub user DBorello as part of his excellent
+ PiSmoker project: https://github.com/DBorello/PiSmoker and modified by GitHub user markalston.
+
+ PID controller based on velocity PID form (type c) https://apmonitor.com/pdc/index.php/Main/ProportionalIntegralDerivative
+   u    = u(k-1) - KP(PV - PV(k-1)) + KIe(k)deltaT - KD ((PV - 2PV(k-1) + PV(k-2))/deltaT) = controller output
+   k    = current measurement index
+   KP   = Proportional Gain
+   KI   = Integral Gain
+   KD   = Derivative Gain
+   PV   = Process Variable (Chamber Temp)
+   e    = error
+
+For this controller it is recommended to perform step-response testing in order to approximate a FOPDT (First order plus dead time)
+model.  This provides Kp, tau, and theta (see velocity PID link above for equations)
+   Kp     = controller gain
+   tau    = process time constant
+   theta  = process time delay
+   lambda = max(1.5 * theta, 0.5 * tau)
+
+IMC-style PID tuning for FOPDT utilized the above parameters to estimate Kc, Ti, and Td:
+    Kc = tau / (Kp*(lam + theta)) = controller gain
+    Ti = tau + theta/2 = integral reset time
+    Td = (tau*theta) / (2*tau + theta) = derivative time constant
+	
+Then, to get the final gains:
+   KP = Kc
+   KI = Kc / Ti
+   KD = Kc * Td
+
+  
+  Configuration Defaults: 
+  "config": {
+      "Kc": 0.0015,
+      "Ti": 0.00000375,
+      "Td": 0.0
+   }
+
+*****************************************
+'''
+
+'''
+Imported Libraries
+'''
+import time
+import logging
+
+from controller.base import ControllerBase 
+
+'''
+Class Definition
+'''
+class Controller(ControllerBase):
+	def __init__(self, config, units, cycle_data):
+		super().__init__(config, units, cycle_data)
+
+		# pb, ti, td
+		self._calculate_gains(config[0], config[1], config[2])
+
+		self.p = 0.0
+		self.i = 0.0
+		self.d = 0.0
+		self.u = 0
+		self.u_1 = None
+		self.pv_1 = None
+		self.pv_2 = None
+		self.initialized = False
+		
+		self.last_update = time.time()
+		self.error = 0.0
+		self.error_last = 0.0
+		self.set_point = 0
+
+		self.derv = 0.0
+		self.inter = 0.0
+
+		# self.set_target(0.0)
+
+	def _calculate_gains(self, kc, ti, td):
+		if kc == 0:
+			self.kp = 0
+		else:
+			self.kp = kc
+		if ti == 0:
+			self.ki = 0
+		else:
+			self.ki = self.kp / ti
+		self.kd = self.kp * td
+
+
+	def update(self, current):
+		# dt
+		dt = time.time() - self.last_update
+
+		# Calculate needed deltas and error
+		d_pv = current - self.pv_1
+		dd_pv = (current - (2.0 * self.pv_1) + self.pv_2)
+		error = self.set_point - current
+		
+        # Calculate P, I, and D portions of equation
+		self.p = self.kp * d_pv
+		self.i = self.ki * error * dt
+		self.d = self.kd * (dd_pv / dt)
+
+		# PID
+		self.u = self.u_1 - self.p + self.i - self.d
+		
+        # Update k-1 and k-2 variables for next loop
+		self.u_1 = self.u
+		self.pv_2 = self.pv_1
+		self.pv_1 = current
+
+		# Clamping anti-windup method. 
+		# Stops integration when the sum of the block components exceeds the output limits 
+		# and the integrator output and block input have the same sign. 
+		# Resumes integration when either the sum of the block components exceeds the output limits 
+		# and the integrator output and block input have opposite sign or the sum no longer exceeds the output limits.
+		# 
+		# Implemented via reversing the addition to self.inter above if we are clamping.		
+		if not ((abs(self.u) >= 1) and (self.i * self.u > 0)):
+			clamping_log = "false"
+		else:
+			clamping_log = "true"
+			self.inter -= error * dt		
+
+		# Update for next cycle
+		self.error_last = error
+		self.last_update = time.time()
+
+		return self.u
+	
+	def initialize(self, u_init, pv_init):
+		self.u_1 = u_init
+		self.pv_1 = pv_init
+		self.pv_2 = pv_init
+
+	def set_target(self, set_point):
+		self.set_point = set_point
+		self.error = 0.0
+		self.inter = 0.0
+		self.derv = 0.0
+		self.last_update = time.time()
+
+	def set_gains(self, pb, ti, td):
+		self._calculate_gains(pb,ti,td)
+
+
+	def get_k(self):
+		return self.kp, self.ki, self.kd
+	
+	def supported_functions(self):
+		function_list = [
+			'update', 
+	        'set_target', 
+	        'get_config', 
+			'set_gains', 
+			'get_k',
+			'initialize'
+        ]
+		return function_list
